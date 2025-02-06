f2d69be180c6fe67e5892ab17a2b53ce9e0ba04f
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-02-06 17:30:36 +0530

    material tailwind is configured navbar work is under process

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className="text-2xl bg-blue-600">Footer</div>;
 };
 
 export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
-/**@type {import('tailwindcss').Config} */
-import withMT from "./tailwind.config";
+// /**@type {import('tailwindcss').Config} */
+// import withMT from "./tailwind.config";
+// export default withMT({
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//     // Add other paths to your content files here
+//   ],
+//   theme: {
+//     extend: {
+//       acentColor: "#FF014F",
+//     },
+//   },
+//   plugins: [],
+// });
+
+import withMT from "@material-tailwind/react/utils/withMT";
+
 export default withMT({
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    // Add other paths to your content files here
-  ],
+  content: [],
   theme: {
     extend: {},
   },
